50613309df78c332b531edd1670a6c577392b6aa
V <v.prithviraj@capgemini.com>
2016-07-14 16:47:56 +0530

    second commit

---
+++ userinteraction.java
@@ -0,0 +1,46 @@
+package com.flp.ems.view;
+import java.util.Scanner;
+import java.util.*;
+
+public class userinteraction {
+	Scanner in=new Scanner(System.in);
+	
+	public void addemployee(){
+		System.out.println("Enter your name");
+		String name=in.next();
+		
+		System.out.println("Enter your Kin_id");
+		int Kin_id=in.nextInt();
+		
+	    System.out.println("Enter your Email id");
+	    String Email_id=in.next();
+	    
+	    System.out.println("Enter your Mobile number");
+	    String number=in.next();
+	    
+	    System.out.println("Enter your address");
+	    String address=in.next();
+	    
+	   // System.out.println("Enter your date of birth");
+	    		
+	}
+	public void modifyemployee()
+	{
+	}
+	public void removeemployee()
+	{
+	}
+	public void searchemployee()
+	{
+	}
+	public void getallemployee()
+	{
+	}
+}
+	
+	    		
+	    		
+	    
+	}
+
+	}
